e22f7aaf621cfc0f1c8d42e773a370accee86317
bakytdev <azimbakyt111@gmail.com>
2023-10-04 20:30:14 +0600

    media

--- js/script.js
+++ js/script.js
@@ -10,11 +10,28 @@ btn.addEventListener("click", () => {
 })
 
 const swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    loop: true,
-    spaceBetween: 30,
+    slidesPerView: 1,
+    spaceBetween: 10,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 1.5,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+        },
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
     },
 });
